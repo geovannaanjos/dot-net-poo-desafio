1e71c572edd2a2685dda543b62db8da5087d0963
Geovanna Anjos <g.dos.santos.anjos@avanade.com>
2024-05-03 11:56:30 -0300

    feat: implementation of methods

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nok = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+Smartphone ip = new Iphone("987654321", "Iphone 15", "987654321", 128);
+
+System.Console.WriteLine("Nokia:");
+nok.Ligar();
+nok.InstalarAplicativo("Snake");
+
+System.Console.WriteLine("\nIphone:");
+ip.ReceberLigacao();
+ip.InstalarAplicativo("WhatsApp");
